4a86354c107f5f8a6baba6caf6553f37d451950c
jk456445 <81965933+jk456445@users.noreply.github.com>
2022-03-04 15:43:32 +0800

    Add files via upload

---
+++ 3.4.1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char arr[][11]={"C langauge","C++","Java"};
+    printf("char arr的大小為=%d\n",sizeof(arr));
+
+    system("pause");
+    return 0;
+
+}
